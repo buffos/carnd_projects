5bf80f54bfe3fe3a2163d1897d0a36ffbe2708fd
Kostas Oreropoulos <kostas.oreopoulos@gmail.com>
2017-07-26 00:12:50 +0300

    feature: Add functions to measure distance of other cars in a lane
    added:
    - Road::distanceInFront(lane) to find the closest vehicle in the lane
    give in front of my car
    - Road::distanceBehind(lane) to find the closest vehicle in the lane
    give behind my car

--- 11.path.planning/src/road.cpp
+++ 11.path.planning/src/road.cpp
@@ -19,3 +19,65 @@ void Road::updateData(json j, int index)
         cars.push_back(newCar);
     }
 }
+
+double Road::distanceInFront(Vehicle car, int lane)
+{
+    // this function will look ahead in the given lane and find the distance to the closest car in front
+    double max_distance = 100.0; // check up to 100 meters
+    double current_s = 9999999.0;
+    for (auto other_car : cars)
+    {
+        if (car.getLane() == other_car.getLane())
+        {
+            if ((car.s > max_s - max_distance) && (car.s < other_car.s) && (other_car.s < max_s))
+            {
+                // both cars are right before the end of the lap , so i take the difference
+                current_s = min(current_s, other_car.s - car.s);
+            }
+            else if ((car.s > max_s - max_distance) && (other_car.s < max_distance))
+            {
+                // the car is just before the end and the other car has just crossed the end
+                // so the distance is what is left to the end (max_s - car.s) and what the other car has traveled (other_car.s)
+                double distance = max_s - car.s + other_car.s;
+                current_s = min(current_s, distance);
+            }
+            else if (car.s <= max_s - max_distance && car.s < other_car.s)
+            {
+                // the cars are in a zone that can be compared
+                current_s = min(current_s, other_car.s - car.s);
+            }
+        }
+    }
+    return current_s;
+}
+
+double Road::distanceBehind(Vehicle car, int lane)
+{
+    // this function will look ahead in the given lane and find the distance to the closest car behind
+    double max_distance = 100.0; // check up to 100 meters
+    double current_s = 9999999.0;
+    for (auto other_car : cars)
+    {
+        if (car.getLane() == other_car.getLane())
+        {
+            if ((other_car.s > max_s - max_distance) && (other_car.s < car.s) && (car.s < max_s))
+            {
+                // both cars are right before the end of the lap , and my car is in front, so i take the difference
+                current_s = min(current_s, car.s - other_car.s);
+            }
+            else if ((other_car.s > max_s - max_distance) && (car.s < max_distance))
+            {
+                // the other car is just before the end and the my car has just crossed the end
+                // so the distance is what is left to the end (max_s - other_car.s) and what my car has traveled (car.s)
+                double distance = max_s - other_car.s + car.s;
+                current_s = min(current_s, distance);
+            }
+            else if (other_car.s <= max_s - max_distance && other_car.s < car.s)
+            {
+                // the cars are in a zone that can be compared and other car is behind
+                current_s = min(current_s, car.s - other_car.s);
+            }
+        }
+    }
+    return current_s;
+}
--- 11.path.planning/src/road.h
+++ 11.path.planning/src/road.h
@@ -11,15 +11,18 @@ using namespace std;
 struct Road
 {
     vector<Vehicle> cars{};
-    const float max_speed = 27.0; // 60mph = 26.82m/s
-    const float target_speed = 23.0; // 50mph = 22.352m/s
+    const float max_s = 6915.0;          // in meters after that its a loop
+    const float max_speed = 27.0;        // 60mph = 26.82m/s
+    const float target_speed = 23.0;     // 50mph = 22.352m/s
     const float max_acceleration = 10.0; // 10m/s^2
-    const float max_jerk = 50.0; // 50/m/s^3
-    const int frames = 50; // 50 frames per second
+    const float max_jerk = 50.0;         // 50/m/s^3
+    const int frames = 50;               // 50 frames per second
     const int lanes = 3;
 
-    inline Road(){}
+    inline Road() {}
     void updateData(json j, int index = 1);
+    double distanceInFront(Vehicle car, int lane);
+    double distanceBehind(Vehicle car, int lane);
 };
 
 #endif // !ROAD_H
